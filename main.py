7a6658b208bdfb53abcfad03a54bd41de2db1fa2
Tinoy Thomas <tinoytpm1@gmail.com>
2022-05-17 16:33:43 -0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,323 @@
+from __future__ import annotations
+
+import random
+from typing import TYPE_CHECKING
+
+import pygame as pg
+from pygame.locals import *  # type: ignore
+
+if TYPE_CHECKING:
+    from pygame.rect import Rect
+    from pygame.surface import Surface
+
+# Colours
+WHITE = (255, 255, 255)
+# Screen size
+SIZE = WIDTH, HEIGHT = (768, 896)
+# Screen caption
+CAPTION = 'Flappy bird'
+# Frames per second
+FPS = 60
+# Frames between new pipes
+PIPES_SPAWN = 180
+# Pipe y offset range
+PIPES_Y_RANGE = [*range(-90, 90)]
+# Note that setting this any higher will probably
+# break visuals and maybe the game
+
+
+class Flappy(pg.sprite.Sprite):
+    """Prepresents the birb"""
+
+    # Type checker complains that these are None (cause of superclass)
+    # So, we declare the type explicitly
+    image: Surface
+    rect: Rect
+
+    def __init__(self) -> None:
+        super().__init__()
+
+        image = pg.image.load('images/flappy.png').convert().convert_alpha()
+        # Images is 3 images in one (I stole it from somewhere)
+        # <flapping_down_image><gliding_image><flapping_up_image>
+        # Here we split it and isolate each part of the image
+        # Dimensions per image are 92x64
+        self.flap_down_image = image.subsurface((0, 0, 92, 64))
+        self.glide_image = image.subsurface((92, 0, 92, 64))
+        self.flap_up_image = image.subsurface((184, 0, 92, 64))
+        # self.image is the image that .draw() will be using
+        # We will be mutating this value to imitate animated flapping
+        self.image = self.glide_image
+        # All images are the same size, this is safe
+        self.rect = self.image.get_rect()
+        # Center the bird
+        self.rect.center = (WIDTH // 2, 200)
+
+        # self.velocity is the speed at which the bird falls or rises
+        # If negative, the bird falls (as y decreases)
+        # If positive, the bird rises (as y increases)
+        self.velocity = -2
+
+        # self.fall_acceleration is the rate at which self.velocity changes
+        # This is simply to make the game feel less static and also
+        # Piss the player off. Flappy bird is meant to be awkward
+        self.fall_acceleration = 1
+
+        # Whether this bird has flown off screen
+        self.dead = False
+
+        # Frames until self.image is cycled
+        self.next_image_in = 6
+
+    def draw(self, screen: Surface) -> None:
+        """Draw this bird onto the game screen"""
+        # Determine speed based on acceleration by velocity
+        speed = self.fall_acceleration * self.velocity
+
+        # Decrease velocity (as the bird is constantly falling)
+        self.velocity -= 0.25
+
+        if self.fall_acceleration < 1.01:
+            # Increase falling acceleration if it is below 1.01
+            self.fall_acceleration += 0.0008
+
+        # Speed has to be opposite because of how this helper works
+        self.rect.move_ip(0, -speed)
+
+        if self.rect.bottom >= HEIGHT or self.rect.top <= -self.rect.height:
+            # The bird went off screen, set .dead to True
+            # Game.tick() knows how to handle this
+            self.dead = True
+
+        if self.next_image_in > 0:
+            # Decrease image counter by 1
+            self.next_image_in -= 1
+        else:
+            # We are updating the image for upcoming frame
+            if self.next_image_in <= 0:
+                # Reset counter
+                self.next_image_in = 8
+
+                if self.fall_acceleration >= 1.01:
+                    # We are accelerating pretty fast here
+                    # Flap the wings harder because bird panic
+                    self.next_image_in = 3
+                # Wing was all the way down, move wing up to middle
+                if self.image == self.flap_down_image:
+                    self.image = self.glide_image
+                # Wing was gliding, move wing all the way up
+                elif self.image == self.glide_image:
+                    self.image = self.flap_up_image
+                # Wing was all the way up, move wing back down
+                elif self.image == self.flap_up_image:
+                    self.image = self.flap_down_image
+
+        screen.blit(self.image, self.rect)
+
+    def flap(self) -> None:
+        """Flap those puny wings once"""
+
+        # .draw() is called soon, set .image to flapping down image
+        self.image = self.flap_down_image
+        # Makes the flapping down image last for 26 frames instead of
+        # the default 8. I know, fancy graphics.
+        self.next_image_in = 26
+        # Set velocity to a positive value so the bird moves up
+        self.velocity = 6.5
+        # Reset acceleration
+        self.fall_acceleration = 1
+
+
+class Pipe(pg.sprite.Sprite):
+    """Represents an upright or flipped pipe"""
+
+    image: pg.surface.Surface
+    rect: pg.rect.Rect
+
+    def __init__(self, image: Surface, offset: int, flipped: bool = False) -> None:
+        super().__init__()
+
+        self.image = image
+        self.rect = image.get_rect()
+        self.rect.centerx = WIDTH + self.rect.width
+
+        if flipped:  # This is an upside down pipe
+            self.rect.top = -85 + offset
+        else:  # This is an upright pipe
+            self.rect.bottom = HEIGHT + 85 + offset
+
+    def draw(self, screen):
+        self.rect.move_ip(-2, 0)
+        screen.blit(self.image, self.rect)
+
+
+class PipePair:
+    """Represents a pair of pipes"""
+
+    def __init__(self, game: Game) -> None:
+        try:
+            # Check if we have images cached
+            image, flipped = PipePair.images
+        except AttributeError:
+            # Images haven't been loaded yet, load and parse them
+            image = pg.image.load('images/pipe.png').convert().convert_alpha()
+            # Resize the image so it doesn't look stupid
+            image = pg.transform.scale(
+                image, (image.get_width() // 1.8, image.get_height() // 1.8)
+            )
+            # Get a 180-flipped version of the image
+            flipped = pg.transform.rotate(image, 180)
+
+            # Cache images for future PipePair objects
+            PipePair.images = image, flipped
+
+        # The y offset for drawing the pipes
+        offset = random.choice(PIPES_Y_RANGE)
+
+        # Represents the two pipes
+        self.upright = Pipe(image, offset)  # This one is upright
+        self.flipped = Pipe(flipped, offset, flipped=True)  # This one is upside down
+
+        # Indicates whether flappy has passed this pipe
+        self.passed = False
+        self.game = game
+
+    def draw(self, screen: pg.surface.Surface) -> None:
+        """Draw this pipe pair onto the game screen"""
+
+        # Draw both pipes
+        self.upright.draw(screen)
+        self.flipped.draw(screen)
+
+        # Check if flappy has passed this pipe
+        if not self.passed:
+            if self.upright.rect.right <= self.game.flappy.rect.left:
+                # Flappy has passed this pipe, increment score and set
+                # .passed to True to prevent this bit from running again
+                self.passed = True
+                self.game.score += 1
+
+        else:
+            # Check whether we are off screen
+            if self.upright.rect.right <= 0:
+                # We are off the screen, deref and let garbage collection
+                # take care of this object
+                self.game.pipes.remove(self)
+
+    def collides(self, flappy: Flappy) -> bool:
+        """Return whether flappy managed to bump into this pipe"""
+        coll = pg.sprite.collide_rect
+        return coll(flappy, self.upright) or coll(flappy, self.flipped)
+
+
+class Game:
+    """Represents the flappy bird game"""
+
+    def __init__(self) -> None:
+        # Initialize pygame
+        pg.display.init()
+        pg.font.init()
+
+        # Create screen
+        self.screen = pg.display.set_mode(SIZE)
+
+        # Game state
+        self.score = 0
+        self.pipes = []
+        self.next_pipe_spawn = PIPES_SPAWN
+
+        # Create sprites
+        self.flappy = Flappy()
+        self.create_pipe()
+
+        # Customize display
+        icon = pg.transform.scale(self.flappy.image, (32, 32))
+        pg.display.set_icon(icon)
+        pg.display.set_caption(CAPTION)
+
+        # Various utilities
+        self.clock = pg.time.Clock()
+        self.font = pg.font.SysFont('Comic Sans MS', 30)
+        self.background = pg.image.load('images/background.png').convert()
+
+    def create_pipe(self) -> None:
+        """Create a ``PipePair`` and append to ``self.pipes``"""
+        self.pipes.append(PipePair(self))
+
+    def tick(self) -> int:
+        """Render a single frame and run the game for a single tick"""
+        # If the bird went off screen, exit
+        if self.flappy.dead:
+            return 0
+
+        # Event handling
+        for event in pg.event.get():
+            # QUIT events will close the game
+            if event.type == QUIT:
+                return 0
+
+            if event.type == KEYDOWN:
+                # Pressing Q or Esc will close the game
+                if event.key in {pg.K_q, pg.K_ESCAPE}:
+                    return 0
+
+                # Pressing SPACE will make flappy flap
+                if event.key == pg.K_SPACE:
+                    self.flappy.flap()
+
+        # Decrement spawn counter by 1 every frame
+        if self.next_pipe_spawn > 0:
+            self.next_pipe_spawn -= 1
+        else:
+            # Spawn a pipe and reset spawn counter
+            self.next_pipe_spawn = PIPES_SPAWN
+            self.create_pipe()
+
+        # Preparing our new frame
+        screen = self.screen
+
+        # Draw the background (screen size is same as background size)
+        screen.blit(self.background, (0, 0))
+
+        # Draw sprites
+        self.flappy.draw(screen)
+        for pipe in self.pipes:
+            pipe.draw(screen)
+
+            # If pipe collides with fappy, close the game
+            if not pipe.passed and pipe.collides(self.flappy):
+                return 0
+
+        # Draw score counter
+        screen.blit(self.font.render(str(self.score), False, WHITE), (10, 0))
+
+        # Update the screen with our new frame
+        pg.display.flip()
+
+        # Maintain a rough framerate of "FPS"
+        self.clock.tick(FPS)
+
+        # Return 1 to indicate successful tick and to keep game open
+        return 1
+
+
+def main():
+    """Run flappy bird"""
+
+    # Create the game
+    game = Game()
+
+    try:
+        # Game loop
+        while True:
+            # If .tick() returns 0, break the loop
+            if not game.tick():
+                break
+
+    # Graceful exit for EOF/Ctrl+C
+    except KeyboardInterrupt:
+        pass
+
+
+if __name__ == '__main__':
+    main()
